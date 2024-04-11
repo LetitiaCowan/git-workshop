d9ae2869cfceac3f7704cba2a8f3dedd415751ea
letitiacowan <letitia.cowan01@hotmail.com>
2024-04-11 11:54:04 +1000

    new-feature complete

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
         {count}
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Increment count 
         </button>
